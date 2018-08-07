76e89784d05247201a8b6d03fdde36960ec82f11
Angel Lugo <this@angellugo.com>
2018-08-07 11:45:44 -0700

    Added more friends to the friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,33 @@
 const friendsArray = [
     {
-        name: 'Angel',
-        photo: 'https://bit.ly/2M5OJhI',
-        scores: ['5','5','5','5','5','5','5','5','5','5']
+        name: 'Joey',
+        photo: 'https://images.pexels.com/photos/1138903/pexels-photo-1138903.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: 'Lisa',
+        photo: 'https://images.pexels.com/photos/247322/pexels-photo-247322.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [4,4,4,4,4,4,4,4,4,4]
+    },
+    {
+        name: 'Chandler',
+        photo: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg?auto=compress&cs=tinysrgb&h=350',
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: 'Ross',
+        photo: 'https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: 'Monica',
+        photo: 'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: 'Rachel',
+        photo: 'https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&h=350',
+        scores: [1,2,3,4,5,5,4,3,2,1]
     }
   ];
   
