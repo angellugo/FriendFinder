756adbbfac466998774cdad04c0cbdbf01f2b417
Angel Lugo <this@angellugo.com>
2018-08-07 11:28:22 -0700

    Added initial data to the friends array

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+const friendsArray = [
+    {
+        name: 'Angel',
+        photo: 'https://bit.ly/2M5OJhI',
+        scores: ['5','5','5','5','5','5','5','5','5','5']
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
