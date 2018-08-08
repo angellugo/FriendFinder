5cd28f259e080214d5539e098d0422ab5257ae4d
Angel Lugo <this@angellugo.com>
2018-08-08 10:45:17 -0700

    Added the logic to find a matching friend upon a post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,38 @@ module.exports = function (app) {
     res.json (friends);
   });
 
-  app.post ('/api/friends', function () {
-    console.log ('Going to post');
+  app.post ('/api/friends', function (request, response) {
+    // create a new friend to push to the friends array
+    const newFriend = {
+      name: request.body.name,
+      photo: request.body.photo,
+      scores: [],
+    };
+
+    // parse and store scores for the new friend
+    request.body.scores.forEach (element => {
+      newFriend.scores.push (parseInt (element));
+    });
+
+    // find  new BFF
+    const totalDifferences = [];
+    friends.forEach (friend => {
+      let totalDifference = 0;
+      for (let index = 0; index < friend.scores.length; index++) {
+        totalDifference += Math.abs (
+          friend.scores[index] - newFriend.scores[index]
+        );
+      }
+      totalDifferences.push (totalDifference);
+    });
+    const smallestDifference = Math.min(...totalDifferences);
+    const indexOfNewBFF = totalDifferences.indexOf(smallestDifference);
+    const newBFF = friends[indexOfNewBFF];
+
+    // respond with the friend with the least difference
+    response.json (newBFF);
+
+    // put the new friend in the friends array
+    friends.push (newFriend);
   });
 };
